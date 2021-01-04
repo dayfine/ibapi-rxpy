bd88c939287f5f201eb2ff71b1c6d56ea0f91289
dayfine <26423504+dayfine@users.noreply.github.com>
2021-01-04 15:21:44 -0800

    add test skeleton for positions

---
+++ ibrx/mess/messages/position_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from hamcrest import assert_that, calling, equal_to, raises
+from ibapi import contract
+from rx import create, empty, from_iterable
+
+
+class PositionTest(unittest.TestCase):
+
+    def test_this(self):
+        # TODO: add test
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
