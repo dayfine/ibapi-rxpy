037f04aa567923a800cd3e17d6bb6e3c65136bfe
dayfine <26423504+dayfine@users.noreply.github.com>
2021-01-06 19:20:43 -0800

    make position work and add minimal test

--- ibrx/mess/messages/open_orders_test.py
+++ ibrx/mess/messages/open_orders_test.py
@@ -1,26 +1,12 @@
 import unittest
 
 from hamcrest import anything, all_of, assert_that, contains_exactly, empty, equal_to, has_property
-from hamcrest.core.base_matcher import BaseMatcher
-from ibapi import contract as ib_contract
 from ibapi import order as ib_order
 from ibapi import order_state as ib_order_state
 from rx import create, empty as empty_obs, from_iterable
 
 from ibrx.mess.message import IbApiMessageType, IbApiMessage
-from ibrx.mess.messages import open_orders
-
-
-def _contract1():
-    contract = ib_contract.Contract()
-    contract.conId = 265598
-    contract.symbol = 'AAPL'
-    contract.secType = 'STK'
-    contract.exchange = 'SMART'
-    contract.currency = 'USD'
-    contract.localSymbol = 'AAPL'
-    contract.tradingClass = 'NMS'
-    return contract
+from ibrx.mess.messages import open_orders, test_utils
 
 
 def _order1():
@@ -39,18 +25,6 @@ def _order1():
 def _order_state1():
     order_state = ib_order_state.OrderState()
     return order_state
-
-
-def _contract2():
-    contract = ib_contract.Contract()
-    contract.conId = 43645865
-    contract.symbol = 'IBKR'
-    contract.secType = 'STK'
-    contract.exchange = 'SMART'
-    contract.currency = 'USD'
-    contract.localSymbol = 'IBKR'
-    contract.tradingClass = 'NMS'
-    return contract
 
 
 def _order2():
@@ -73,12 +47,6 @@ def _order_state2():
 
 class OpenOrderTest(unittest.TestCase):
 
-    def _assert_eq_open_orders(self, actual, expected):
-        assert_that(actual.order_id, equal_to(expected.order_id))
-        assert_that(actual.contract, equal_to(expected.contract))
-        assert_that(actual.order, equal_to(expected.order))
-        assert_that(actual.order_state, equal_to(expected.order_id))
-
     def test_empty_observable(self):
         obs = open_orders.collect(empty_obs())
 
@@ -88,10 +56,10 @@ class OpenOrderTest(unittest.TestCase):
         obs = open_orders.collect(
             from_iterable([
                 IbApiMessage(type=IbApiMessageType.OPEN_ORDER,
-                             payload=(0, _contract1(), _order1(),
+                             payload=(0, test_utils.appl_contract(), _order1(),
                                       _order_state1())),
                 IbApiMessage(type=IbApiMessageType.OPEN_ORDER,
-                             payload=(15, _contract2(), _order2(),
+                             payload=(15, test_utils.ibkr_contract(), _order2(),
                                       _order_state2())),
             ]))
 
