4f8573a20db8c2d2cafc4c0dc2479bceea7c4fac
dayfine <26423504+dayfine@users.noreply.github.com>
2021-01-13 21:14:27 -0800

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ibrx",
-    version="0.0.1",
+    version="0.0.2",
     author="dayfine",
     author_email="",
     description="IB API using RxPY",
